0ad149c969843c2058a50789e7481ad2da4afe1a
Hanna-Nagrehelna <anna.nagrebelna@gmail.com>
2024-08-19 02:19:11 +0300

    modificate errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -12,7 +12,8 @@ const errorHandler = (err, _req, res, _next) => {
 
   res.status(500).json({
     status: 500,
-    message: 'Something went wrong'
+    message: 'Something went wrong',
+    data: err.message,
   });
 };
 
