81293c8e7b9820e527c9304168d0397f208fccce
Hanna-Nagrehelna <anna.nagrebelna@gmail.com>
2024-08-20 15:36:59 +0300

    modificate models\contact.js

--- src/models/contact.js
+++ src/models/contact.js
@@ -1,14 +1,35 @@
 import mongoose from 'mongoose';
 
-const contactSchema = new mongoose.Schema({
-    name: String,
-    phoneNumber: String,
-    email: String,
-    isFavourite: Boolean,
-    contactType: String,
-});
+const contactSchema = new mongoose.Schema(
+    {
+        name: {
+        type: String,
+        required: true,
+        },
+        phoneNumber: {
+        type: String,
+        required: true,
+        },
+        email: {
+        type: String,
+        required: true,
+        },
+        isFavourite: {
+        type: Boolean,
+        required: true,
+        default: false,
+        },
+        contactType: {
+        type: String,
+        required: true,        
+        },
+    },
+    {
+    timestamps: true,
+  },
+);
 
 // Створення моделі
-const Contact = mongoose.model('Contact', contactSchema);
+const Contact = mongoose.model('Contact', contactSchema); // collection: contacts
 
 export default Contact;
